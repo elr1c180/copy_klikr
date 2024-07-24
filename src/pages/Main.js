f184276a26a4c9bebae8fc3056e08a891a34484c
elr1c180 <elr1c.edw180@gmail.com>
2024-07-24 21:38:55 +0400

    rewards_fkn_final

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,9 +1,18 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import MainComponent from "../components/main/Main";
+import { useEffect } from "react";
+
 
 const Main = () => {
-    
+    useEffect(() => {
+        // Подключаем Telegram Web App SDK
+        if (window.Telegram && window.Telegram.WebApp) {
+          window.Telegram.WebApp.ready(function() {
+            window.Telegram.WebApp.expand();
+          });
+        }
+      }, []);
     return (
         <div>
             <MainComponent/>
