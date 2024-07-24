ff9e36f2c369856671d17555d45d81b3c02f22a6
elr1c180 <elr1c.edw180@gmail.com>
2024-07-24 22:40:04 +0400

    vibration

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -3,6 +3,8 @@ import Navbar from "../components/Navbar/Navbar";
 import MainComponent from "../components/main/Main";
 
 const Main = () => {
+    window.Telegram.WebApp.setBackgroundColor('#000000');
+    window.Telegram.WebApp.setHeaderColor('#ffffff');
     return (
         <div>
             <MainComponent/>
