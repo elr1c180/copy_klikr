15711961ef9be264439396a04e9198279eb46780
elr1c180 <elr1c.edw180@gmail.com>
2024-07-24 21:32:45 +0400

    rewards_fkn_final

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -3,6 +3,9 @@ import Navbar from "../components/Navbar/Navbar";
 import MainComponent from "../components/main/Main";
 
 const Main = () => {
+    window.Telegram.WebApp.ready(function() {
+        window.Telegram.WebApp.expand();
+    });
     return (
         <div>
             <MainComponent/>
