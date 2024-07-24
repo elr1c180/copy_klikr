142a7b6f68afb41952b330f5ca8dc9244c5fe12f
elr1c180 <elr1c.edw180@gmail.com>
2024-07-24 22:07:30 +0400

    rewards_fkn_final

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import cl from './Main.module.css'
-import clicker from './dollar.png'
-import energy from './lightning.png'
-import top from './chart.png'
+import cl from './Main.module.css';
+import clicker from './dollar.png';
+import energy from './lightning.png';
+import top from './chart.png';
 import { useState, useEffect } from "react";
 
 const MainComponent = () => {
@@ -14,14 +14,18 @@ const MainComponent = () => {
 
     const handleClick = (event) => {
         if (energyCount > 0) {
-        setClickCount(prevClickCount => prevClickCount + 1);
-        setEnergy(prevEnergyCount => prevEnergyCount - 1);
-        
-        window.navigator.vibrate(100); 
+            setClickCount(prevClickCount => prevClickCount + 1);
+            setEnergy(prevEnergyCount => prevEnergyCount - 1);
+
+            // Вибрация при клике
+            if (navigator.vibrate) {
+                navigator.vibrate(50); // 50 миллисекунд вибрации
+            }
+
         } else {
-        window.Telegram.WebApp.showAlert("Energy is lost!");
+            window.Telegram.WebApp.showAlert("Energy is lost!");
         }
-        
+
         setIsClicked(true);
         setCounter(prevCounter => prevCounter + 1);
 
@@ -30,18 +34,18 @@ const MainComponent = () => {
         const offsetY = event.touches ? event.touches[0].clientY - boundingRect.top : event.clientY - boundingRect.top;
 
         setClickPositions(prevClickPositions => [
-        ...prevClickPositions, 
-        { x: offsetX, y: offsetY, id: counter }
+            ...prevClickPositions, 
+            { x: offsetX, y: offsetY, id: counter }
         ]);
 
         setTimeout(() => {
-        setIsClicked(false);
+            setIsClicked(false);
         }, 500);
     };
 
     useEffect(() => {
         const interval = setInterval(() => {
-        setClickPositions(prevClickPositions => prevClickPositions.filter(pos => pos.id !== counter - 10));
+            setClickPositions(prevClickPositions => prevClickPositions.filter(pos => pos.id !== counter - 10));
         }, 1000);
 
         return () => clearInterval(interval);
@@ -52,7 +56,7 @@ const MainComponent = () => {
             <div className={cl.Leadersboard}>
                 <div className={cl.Place}>
                     <img src={top} alt="top"/>
-                    <span>Place 27k <i class="fa-solid fa-chevron-right"></i></span>
+                    <span>Place 27k <i className="fa-solid fa-chevron-right"></i></span>
                 </div>
                 <span className={cl.End}>Ends in</span>
             </div>
@@ -63,18 +67,18 @@ const MainComponent = () => {
             <div className={cl.Clicker}>
                 <img src={clicker} className={`${isClicked ? cl.Clicked : ''}`} alt="" onTouchStart={handleClick}/>
                 {clickPositions.map((pos) => (
-          <div
-            key={pos.id}
-            className={cl.clickCounter}
-            style={{ top: `${pos.y}px`, left: `${pos.x+80}px` }}
-          >
-            +1
-          </div>
-        ))}
+                    <div
+                        key={pos.id}
+                        className={cl.clickCounter}
+                        style={{ top: `${pos.y}px`, left: `${pos.x+80}px` }}
+                    >
+                        +1
+                    </div>
+                ))}
             </div>
 
             <div className={cl.Energy}>
-                <label for="enegry"> <img src={energy} alt="energy"/> <span>{energyCount}</span></label>
+                <label htmlFor="energy"> <img src={energy} alt="energy"/> <span>{energyCount}</span></label>
                 <progress id="energy" className={cl.energyBar} max="2000" value={energyCount}></progress>
             </div>
         </div>
