dcf168ab638b36827732fc49ff660faadf09ffc6
elr1c180 <elr1c.edw180@gmail.com>
2024-07-28 16:08:20 +0400

    first_try

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -4,28 +4,12 @@ import clicker from './dollar.png';
 import energy from './lightning.png';
 import top from './chart.png';
 import { useState, useEffect } from "react";
+import axios from 'axios';
 
 const MainComponent = () => {
 
-    async function checkOrCreateUser() {
-        const chatId = window.Telegram.WebApp.initDataUnsafe.user.id;
-        const username = window.Telegram.WebApp.initDataUnsafe.user.username; 
     
-        try {
-            const response = await axios.post('http://162.213.249.107:9000/api/check_or_create_user/', {
-                chat_id: chatId,
-                username: username
-            });
-    
-            console.log(response.data.message);
-    
-        } catch (error) {
-            console.error('Error making the request:', error);
-        }
-    }
-    
-    checkOrCreateUser();
-    
+
     const [clickCount, setClickCount] = useState(0);
     const [energyCount, setEnergy] = useState(2000);
     const [isClicked, setIsClicked] = useState(false);
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,11 +2,30 @@ import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import MainComponent from "../components/main/Main";
 
-import axios from 'axios';
-
 const Main = () => {
     window.Telegram.WebApp.setBackgroundColor('#FFFFFF');
     window.Telegram.WebApp.setHeaderColor('#000000');
+
+    async function sendTestData() {
+        const userData = {
+            chat_id: window.Telegram.WebApp.initDataUnsafe.user.id,
+            username: window.Telegram.WebApp.initDataUnsafe.user.username
+        };
+    
+        try {
+            const response = await axios.post('http://162.213.249.107:9000/api/check_or_create_user/', userData);
+            
+            if (response.data && response.data.message) {
+                console.log('Ответ от сервера:', response.data.message);
+            } else {
+                console.error('Неизвестный формат ответа от сервера:', response);
+            }
+        } catch (error) {
+            console.error('Ошибка при записи тестовых данных:', error);
+        }
+    }
+    
+    sendTestData();
 
     return (
         <div>
