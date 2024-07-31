95a68b21cd55207965e30a74136f2855901415d5
elr1c180 <elr1c.edw180@gmail.com>
2024-07-31 20:41:09 +0400

    profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -6,13 +6,25 @@ import { useState } from "react";
 const Profile = () => {
     const [userId, setUserId] = useState('');
     const [userPhoto, setUserPhoto] = useState('');
+
+    const navigate = useNavigate();
+    var BackButton = window.Telegram.WebApp.BackButton;
+    BackButton.show();
+
+    BackButton.onClick(function() {
+        BackButton.hide();
+    });
+    window.Telegram.WebApp.onEvent('backButtonClicked', function() {
+        navigate('/main');
+    });
+
     useEffect(() => {
 
         if (window.Telegram && window.Telegram.WebApp) {
             const user = window.Telegram.WebApp.initDataUnsafe?.user;
             if (user) {
                 setUserId(user.username || `${user.first_name} ${user.last_name}`);
-                setUserPhoto(user.photo_url)
+                setUserPhoto(user.photo_url);
             }
         }
     }, []);
