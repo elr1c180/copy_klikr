984fddbc21905f4f56991d9ce2a8cbc5044ebfc9
elr1c180 <elr1c.edw180@gmail.com>
2024-07-31 18:14:57 +0400

    boost

--- src/pages/Boost.js
+++ src/pages/Boost.js
@@ -2,6 +2,9 @@ import React from "react";
 import cl from './src/Boost/Boost.module.css'
 import { Link } from "react-router-dom";
 
+import tg from './src/Boost/telegram.png'
+import X from './src/Boost/twitter.png'
+
 const Boost = () => {
     return (
         <div className={cl.boostWrap}>
@@ -11,15 +14,33 @@ const Boost = () => {
                         Join Telegram Chat
                     </span>
                     <span className={cl.Reward}>
-                        50,000
+                        5,000
                     </span>
                 </div>
                 
                 <Link to="https://t.me/QuickClickOfficialBot" className={cl.Button}>
-                        <h6>Join</h6>
+                <img src={tg} alt="telegram"/><h6>Join</h6>
                 </Link>
                 <div className={cl.Description}>
                     <span>Join the Telegram Chat to earn points</span>
+                </div>
+            </div>
+
+            <div className={cl.baseBlock}>
+                <div className={cl.Header}>
+                    <span className={cl.Title}>
+                        Follow Twitter(X) Account
+                    </span>
+                    <span className={cl.Reward}>
+                        5,000
+                    </span>
+                </div>
+                
+                <Link to="https://t.me/QuickClickOfficialBot" className={cl.Button}>
+                        <img src={X} alt="twitter"/> <h6>Follow</h6>
+                </Link>
+                <div className={cl.Description}>
+                    <span>Follow the Twitter Account to earn points</span>
                 </div>
             </div>
         </div>
