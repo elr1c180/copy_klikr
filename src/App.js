b050442ebce46044c0be5471b77d715e21a72fbf
elr1c180 <elr1c.edw180@gmail.com>
2024-07-31 20:23:44 +0400

    profile

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes, Navigate, BrowserRouter } from 'react-router-dom';
 import Main from './pages/Main';
 import Rewards from './pages/Rewards';
 import Boost from './pages/Boost';
+import Profile from './pages/Profile';
 
 function App() {
   const tele = window.Telegram.WebApp;
@@ -14,6 +15,7 @@ function App() {
                     <Route path="/" element={<Main />} />
                     <Route path="/main" element={<Main />} />
                     <Route path="/boost" element={<Boost />} />
+                    <Route path="/Profile" element={<Profile />} />
                     <Route path="/rewards" element={<Rewards />} />
                     <Route path="*" element={<Navigate to="/" />} />
                 </Routes>
